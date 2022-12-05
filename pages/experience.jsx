1c2af9559f697d23bc3e3a4f25b61ffb542bb771
Shubham Dattatray Adhane <adhaneshubham622@gmail.com>
2022-12-05 17:09:46 +0530

    add

---
+++ pages/experience.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const experience = () => {
+  return (
+    <div>
+      <h1 style={{textAlign:"center", backgroundColor:"teal"}}>Experience</h1>
+    </div>
+  )
+}
+
+
+
+export default experience
